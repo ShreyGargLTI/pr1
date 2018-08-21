ad5fc4f324037e67ed2952995d8ebcc83cdb1a49
ShreyGargLTI <shrey.garg@lntinfotech.com>
2018-08-21 23:53:56 +0530

    First Commit

---
+++ frst.ps1
@@ -0,0 +1,30 @@
+﻿function ch
+{
+    param([string[]] $lst, [string] $val)
+    $flag = $true
+    foreach($v in $lst)
+    {
+        if($v -eq $val)
+        {
+            $flag =$false
+        }
+    }
+
+    return $flag
+}
+function addCh 
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)] [string[]] $lst, 
+        [parameter(Mandatory = $true)] [ValidateScript({ch $lst $val})] [string] $val
+        )
+
+        Write-Host "passed"
+}
+$a = "berlin"
+$b = "london"
+$list = "berlin", "london", "delhi", "patna", "new york"
+$flag = $false
+
+addch $list $a
